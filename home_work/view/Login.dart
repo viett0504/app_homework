174ae28b05c365b6981497e96a14b75caa826043
viett0504 <honmatroi@gmail.com>
2025-02-26 14:38:11 +0700

    Update Login.dart

--- home_work/view/Login.dart
+++ home_work/view/Login.dart
@@ -1,21 +1,66 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: HomeWork1(),
   ));
 }
 
-class HomeWork1 extends StatelessWidget {
+class HomeWork1 extends StatefulWidget {
   const HomeWork1({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<HomeWork1> createState() => _HomeWork1State();
+}
+class _HomeWork1State extends State<HomeWork1> {
+  final TextEditingController phoneController = TextEditingController();
+  String? errorText;
+  Timer? _time;
+
+  @override
+  void initState() {
+    super.initState();
+    phoneController.addListener(_onPhoneChanged);
+  }
+
+  @override
+  void dispose() {
+    phoneController.removeListener(_onPhoneChanged);
+    phoneController.dispose();
+    _time?.cancel();
+    super.dispose();
+  }
+
+  void _onPhoneChanged() {
+    if (_time?.isActive ?? false) _time!.cancel(); // Hủy timer nếu đang chạy
+    _time = Timer(const Duration(seconds: 1), () {
+      setState(() {
+        String phoneNumber = phoneController.text.trim();
+
+        if (phoneNumber.isEmpty) {
+          errorText = null;
+        } else if (!isValidNum(phoneNumber)) {
+          errorText = "Your phone number is incorrect!";
+        } else {
+          errorText = null;
+        }
+      });
+    });
+  }
+
+
+  bool isValidNum(String phoneNum) {
+    final RegExp phoneReg = RegExp(r'^(?:\+84|0)([3|5|7|8|9])[0-9]{8}$');
+    return phoneReg.hasMatch(phoneNum);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor:const Color(0xDFDFDFDF),
+        backgroundColor: const Color(0xDFDFDFDF),
         toolbarHeight: 50,
         title: const Text(
           'Sign in',
@@ -26,70 +71,77 @@ class HomeWork1 extends StatelessWidget {
           ),
         ),
       ),
-      body:  SingleChildScrollView(
+      body: SingleChildScrollView(
+        physics: AlwaysScrollableScrollPhysics(),
         padding: EdgeInsets.only(top: 50),
         child:
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Enter your phone number",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Enter your phone number",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
-                const SizedBox(
-                  height: 15,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Text(
+                "Enter your phone number to sign in or sign up in this app",
+                style: TextStyle(
+                  fontSize: 15,
                 ),
-                Text(
-                  "Enter your phone number to sign in or sign up in this app",
-                  style: TextStyle(
-                    fontSize: 15,
-                  ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: phoneController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Your phone number',
+                  border: const OutlineInputBorder(),
+                  errorText: errorText,
                 ),
-                const SizedBox(
-                  height: 20,
-                ),
-                TextField(
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    labelText: 'Your phone number',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(
-                  height: 550,
-                ),
-                   SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ButtonStyle(
-                        backgroundColor: WidgetStateProperty.all<Color>(
-                            Colors.blue),
-                        shape: WidgetStateProperty.all<
-                            RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                      ),
-                      child: const Text(
-                        'Confirm',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
+              ),
+              SizedBox(
+                height: 550,
+              ),
+              AnimatedPadding(
+                duration: const Duration(milliseconds: 300),
+                padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: (){},
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all<Color>(
+                          Colors.blue),
+                      shape: WidgetStateProperty.all<
+                          RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
                         ),
                       ),
                     ),
+                    child: const Text(
+                      'Confirm',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
-              ],
-            ),
+                ),
+              ),
+            ],
           ),
+        ),
       ),
 
     );
